26ec87d849958737ce3c2087daec58ae9e42ff66
KaasJ <jorritstein@gmail.com>
2018-10-12 23:39:55 +0200

    identation

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,22 @@
-
-
-
-
 var aboutMe = document.getElementById('aboutme');
 
-aboutMe.onclick = function () {
+aboutMe.onclick = function() {
   document.getElementById('hiddenelement1').style.display = "block";
- }
+};
 
+var mySelf = document.getElementById('jas');
 
-
-  var mySelf = document.getElementById('jas');
-
-  mySelf.onclick = function () {
-    document.getElementById('hiddenelement1').style.display = "block";
-   }
-
-
-
+mySelf.onclick = function() {
+  document.getElementById('hiddenelement1').style.display = "block";
+};
 
 var myImage = document.getElementById('jorrit');
 
 myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/ik.png') {
-      myImage.setAttribute ('src','images/gokyo.jpeg');
-    } else {
-      myImage.setAttribute ('src','images/ik.png');
-    }
-}
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/ik.png') {
+    myImage.setAttribute('src', 'images/gokyo.jpeg');
+  } else {
+    myImage.setAttribute('src', 'images/ik.png');
+  }
+};
